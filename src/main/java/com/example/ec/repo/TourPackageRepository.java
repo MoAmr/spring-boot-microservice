e75aeb1312ae83f5dab8ab081d4d23c1e2e4765e
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-06-01 18:03:32 +0200

    Controlling API exposure. closes #5

--- src/main/java/com/example/ec/repo/TourPackageRepository.java
+++ src/main/java/com/example/ec/repo/TourPackageRepository.java
@@ -2,6 +2,8 @@ package com.example.ec.repo;
 
 import com.example.ec.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Optional;
 
@@ -10,6 +12,8 @@ import java.util.Optional;
  *
  * Created by Mary Ellen Bowman
  */
+
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
 
     /**
@@ -19,5 +23,29 @@ public interface TourPackageRepository extends CrudRepository<TourPackage, Strin
      * @return Optional of TourPackage
      */
     Optional<TourPackage> findByName(String name);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(String s);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(TourPackage tourPackage);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends TourPackage> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
 
--- src/main/java/com/example/ec/repo/TourRepository.java
+++ src/main/java/com/example/ec/repo/TourRepository.java
@@ -5,8 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-
-import java.util.List;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 
 /**
@@ -17,4 +16,28 @@ import java.util.List;
 public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
 
     Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour tour);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
