306d57c3bdc689e55d5ab0fcd8ca0c4e3f20acd8
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-06-01 17:54:41 +0200

    Adding Paging and sorting to TourRepository. closes #4

--- src/main/java/com/example/ec/repo/TourRepository.java
+++ src/main/java/com/example/ec/repo/TourRepository.java
@@ -1,7 +1,9 @@
 package com.example.ec.repo;
 
 import com.example.ec.domain.Tour;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -12,7 +14,7 @@ import java.util.List;
  *
  * Created by Mary Ellen Bowman
  */
-public interface TourRepository extends CrudRepository<Tour, Integer> {
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
 
-    List<Tour> findByTourPackageCode(@Param("code") String code);
+    Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 }
